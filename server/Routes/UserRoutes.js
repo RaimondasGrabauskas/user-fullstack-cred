d1d2157489aca8d32afdcf6aa7df1c4cbb5bd266
Raimis <rggrabauskas@gmail.com>
2021-07-28 09:30:55 +0300

    added http.rest, create new user router

--- server/Routes/UserRoutes.js
+++ server/Routes/UserRoutes.js
@@ -1,6 +1,28 @@
 const express = require('express');
-
+const User = require('../modules/userModule');
 const router = express.Router();
+
+
+// create new user route
+
+router.post('/api/user/new', async (req, res) => {
+  const newUserDetails = {
+    name: 'John',
+    age: 25,
+    email: 'john@gmail.com',
+    password: 'codeacademy',
+  };
+
+  const newUser = new User(newUserDetails);
+
+  try {
+    const creatingNewUserResult = await newUser.save();
+    res.json(creatingNewUserResult);
+  } catch (err) {
+    res.status(500).json(err)
+  }
+
+});
 
 
 
