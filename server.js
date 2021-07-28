ec5dd3b3fd97a300d056e3030641462a8ab0e87f
Raimis <rggrabauskas@gmail.com>
2021-07-28 09:13:04 +0300

    added env, mongoose connected

--- server.js
+++ server.js
@@ -7,9 +7,21 @@ const morgan = require('morgan');
 const app = express();
 const PORT = 4000;
 
+mongoose
+  .connect(process.env.MONGO_CONNECT_STIRNG, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log('Conneced to Mongoosee');
+  })
+  .catch((err) => console.error(err.message))
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 
+
+
+app.get('/', (req, res) => {
+    res.status(200).json(`Serveris veikia ant porto ${PORT}`);
+});
 
 app.listen(PORT, console.log(`server running on port ${PORT}`));
