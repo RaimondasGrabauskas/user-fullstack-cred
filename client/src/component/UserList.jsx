bb66fc4d266c0828d83a74d02c0ac3b7cfbdf237
Raimis <rggrabauskas@gmail.com>
2021-07-28 09:53:00 +0300

    added user.jsx + userForm.jsx, userList.jsx

---
+++ client/src/component/UserList.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class UserList extends Component {
+
+  constructor(props) {
+    super(props) 
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        MyForm
+      </div>
+    );
+  };
+}
+
+export default UserList;
